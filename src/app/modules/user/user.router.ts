65ce44dce36ce0dfe0d962c78c8b6986e312ec44
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2023-11-25 15:42:57 +0600

    add and get route swagger

--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -1,3 +1,124 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Name of the user
+ *         email:
+ *           type: string
+ *           description: Email of the user
+ *         password:
+ *           type: string
+ *           description: Password of the user
+ *      
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management and retrieval
+ */
+
+/**
+ * @swagger
+ * /:
+ *   post:
+ *     summary: Create a new user
+ *     requestBody:
+ *       description: User data to create a new user
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             userId: 123
+ *             username: john_doe
+ *             password: secret123
+ *             fullName:
+ *               firstName: John
+ *               lastName: Doe
+ *             age: 30
+ *             email: john.doe@example.com
+ *             isActive: true
+ *             hobbies: ['Reading', 'Traveling']
+ *             address:
+ *               street: 123 Main St
+ *               city: Anytown
+ *               country: USA
+ *            
+ * 
+ *     responses:
+ *       '201':
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: true
+ *               message: User created successfully!
+ *               data:
+ *                 _id: 1234567890
+ *                 firstName: John
+ *                 lastName: Doe
+ *                 email: john.doe@example.com
+ *                 hobbies: ['Reading', 'Traveling']
+ *       '400':
+ *         description: Failed to create user
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: false
+ *               message: Failed to create user!
+ *               error:
+ *                 code: 400
+ *                 description: Error message here
+ */
+
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Get all users
+ *     responses:
+ *       '200':
+ *         description: Users fetched successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: true
+ *               message: Users fetched successfully!
+ *               data:
+ *                 - _id: 1234567890
+ *                   firstName: John
+ *                   lastName: Doe
+ *                   email: john.doe@example.com
+ *                   hobbies: ['Reading', 'Traveling']
+ *                 - _id: 0987654321
+ *                   firstName: Jane
+ *                   lastName: Doe
+ *                   email: jane.doe@example.com
+ *                   hobbies: ['Hiking', 'Painting']
+ *       '400':
+ *         description: Failed to fetch users
+ *         content:
+ *           application/json:
+ *             example:
+ *               success: false
+ *               message: Failed to fetch users!
+ *               error:
+ *                 code: 400
+ *                 description: Error message here
+ */
+
+
 import { Router } from 'express';
 import {
   addOrders,
@@ -13,6 +134,7 @@ import {
 const router = Router();
 
 // POST endpoint for creating a new user
+
 router.post('/', createUsers);
 
 // GET endpoint for getting all users
--- src/swaggerConfig.ts
+++ src/swaggerConfig.ts
@@ -3,13 +3,37 @@ import { Options as SwaggerOptions } from 'swagger-jsdoc';
 
 const swaggerOptions: SwaggerOptions = {
   swaggerDefinition: {
+    openapi: '3.0.0',
     info: {
-      title: 'Simple API Documentation',
+      title: 'Bangla_Bazar API',
       version: '1.0.0',
-      description: 'This is a simple REST API application made with Express. It has CRUD operations for users.',
-    },
-  },
-  apis: ['src/routes/*.ts'],
+      description: 'Bangla_Bazar API Information Documentation ',
+      contact: {
+        name: 'Shaiadul Bashar',
+        email: 'mdshaiadul@gmail.com',
+       },
+     },
+    servers: [
+      {
+        url: 'http://localhost:5000/api/users',
+        description: 'Development server',
+      },
+      {
+         url: 'https://banglabazar.vercel.app/api/users',
+         description: 'Production server',
+      }
+    ],
+    components: {
+     securitySchemes: {
+       apiKeyAuth: {
+         type: 'apiKey',
+         in: 'header', // You can also use 'query' or 'cookie'
+         name: 'Authorization', // The name of the header, query parameter, or cookie to be used for the API key
+       },
+     },
+   },
+   },
+  apis: ['src/app/modules/**/*.ts'],
 };
 
 export default swaggerOptions;
